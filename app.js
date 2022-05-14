39c8eafd79095aa78acb8eb21b5ea0e760d5727e
yama <t21772ry@sfc.keio.ac.jp>
2022-05-14 18:18:28 +0900

    a

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ const bcrypt = require('bcrypt');
 const path = require('path');
 const ejs = require("ejs");
 const cool = require('cool-ascii-faces');
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+// const { Pool } = require('pg');
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
 const PORT = process.env.PORT || 5000;
 console.log("port", PORT);
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(express.urlencoded({extended: false}));
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
   // .get('/db', async (req, res) => {
